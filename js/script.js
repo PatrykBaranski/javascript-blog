5df3bb902d9e6015c4865b55ff1ca740265a30c6
Patryk Barański <pbaranski740@gmail.com>
2023-09-14 21:32:49 +0200

    Cleaning dynamic generate tags for code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 "use strict";
 const articles = document.querySelectorAll(".post");
-const titleClickhandler = function (e) {
+
+const removeActiveClass = (htmlElements) =>
+  htmlElements.forEach((element) => element.classList.remove("active"));
+const titleClickHandler = function (e) {
   e.preventDefault();
   const clickedElement = this;
   const activeLinks = document.querySelectorAll(".titles a.active");
   const activeArticles = document.querySelectorAll(".post");
-  const removeActiveClass = (htmlElements) =>
-    htmlElements.forEach((element) => element.classList.remove("active"));
 
   removeActiveClass(activeLinks);
   removeActiveClass(activeArticles);
@@ -15,6 +16,20 @@ const titleClickhandler = function (e) {
     .querySelector(clickedElement.getAttribute("href"))
     .classList.add("active");
 };
+
+const tagClickHandler = function (e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const tag = href.slice(5);
+  const activeTags = document.querySelectorAll(".post-tags .list .active");
+  console.log(activeTags);
+  removeActiveClass(activeTags);
+  document.querySelectorAll(".post-tags .list a").forEach((tag) => {
+    if (tag.getAttribute("href") === href) tag.classList.add("active");
+  });
+  generateTitleLinks(`[data-tags~=${tag}]`);
+};
+
 const generateTitleLinks = function () {
   const linkList = document.querySelector(".titles");
   linkList.innerHTML = "";
@@ -27,18 +42,25 @@ const generateTitleLinks = function () {
               </li>`;
   });
   const links = document.querySelectorAll(".titles a");
-  links.forEach((link) => link.addEventListener("click", titleClickhandler));
+  links.forEach((link) => link.addEventListener("click", titleClickHandler));
 };
 const generateTags = function () {
   articles.forEach((article) => {
     const postTags = article.querySelector(".post-tags .list");
     postTags.innerHTML = "";
     const tagList = article.getAttribute("data-tags").split(" ");
-    tagList.forEach((tag) => {
-      console.log(tag);
-      postTags.innerHTML += `<li><a href="#">${tag}</a></li> `;
-    });
+    tagList.forEach(
+      (tag) =>
+        (postTags.innerHTML += `<li><a href="#tag-${tag}">${tag}</a></li> `),
+    );
+    ``;
   });
+};
+const addClickListenersToTags = function () {
+  document
+    .querySelectorAll(".post-tags .list a")
+    .forEach((tag) => tag.addEventListener("click", tagClickHandler));
 };
 generateTitleLinks();
 generateTags();
+addClickListenersToTags();
